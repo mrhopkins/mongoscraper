ffdb59ac6ebaa2068a0171acbc911f8756691022
Matthew Hopkins <mattrhopkins@gmail.com>
2019-04-22 12:46:39 -0600

    added dependencies

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
+// Dependencies
+var express = require("express");
+var express-handlebars = require("express-handlebars");
+var mongoose = required("mongoose");
+// Require axios and cheerio. This makes the scraping possible
+var cheerio = require("cheerio");
+var axios = require("axios");
+
+// Initialize Express
+var app = express();
+
+// Database configuration
+var databaseUrl = "mongoHeadlines";
+var collections = ["scrapedData"];
+
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
